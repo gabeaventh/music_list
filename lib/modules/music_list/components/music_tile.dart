a43934adb7b71f10b14ec30c1a205744047ec518
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-01-16 18:18:57 +0700

    add: make music tile for songs

---
+++ lib/modules/music_list/components/music_tile.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/models/music.dart';
+
+class MusicTile extends StatelessWidget {
+  final Music music;
+  final void Function(Music) onTap;
+  final bool isPlaying;
+  const MusicTile({
+    Key? key,
+    required this.music,
+    required this.isPlaying,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: NetworkImage(
+              music.artworkUrl100 ?? "",
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      title: Text(music.trackName ?? ""),
+      subtitle: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(music.collectionName ?? ""),
+          SizedBox(
+            height: 5,
+          ),
+          Text(music.artistName ?? ""),
+        ],
+      ),
+      isThreeLine: true,
+      trailing: isPlaying ? Icon(Icons.volume_up) : null,
+      onTap: () => onTap(music),
+    );
+  }
+}
