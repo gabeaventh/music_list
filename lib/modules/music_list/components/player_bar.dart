f505d4fe65f2fe51aaad588eea6034976df4b0b5
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-01-16 18:20:22 +0700

    add: player bar for toggling play, pause and stop button

---
+++ lib/modules/music_list/components/player_bar.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/models/music.dart';
+import 'package:music_player/modules/music_list/bloc/player/player_bloc.dart';
+
+class PlayerBar extends StatelessWidget {
+  final MusicPlayerState musicPlayerState;
+  final void Function(Music) onResume;
+  final void Function(Music) onPause;
+  final VoidCallback onStop;
+  const PlayerBar({
+    Key? key,
+    required this.musicPlayerState,
+    required this.onResume,
+    required this.onPause,
+    required this.onStop,
+  }) : super(key: key);
+
+  Icon mapIcon() {
+    return musicPlayerState.maybeWhen(
+      playing: (music) => Icon(Icons.pause),
+      paused: (music) => Icon(Icons.play_arrow),
+      orElse: () => Icon(Icons.play_arrow),
+    );
+  }
+
+  Function? mapOnPress() {
+    return musicPlayerState.maybeWhen(
+      playing: (music) {
+        onPause(music!);
+      },
+      paused: (music) {
+        onResume(music!);
+      },
+      orElse: () => onPause,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        height: 80,
+        margin: EdgeInsets.only(top: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(0, 1),
+              blurRadius: 6,
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 50,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: mapIcon(),
+                      onPressed: mapOnPress,
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.stop,
+                      ),
+                      onPressed: onStop,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
