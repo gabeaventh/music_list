fd7c24bbcfdf1b49858a56d183d169d74551c180
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-01-16 18:33:28 +0700

    add: MusicListView layout with description below
    - add SearchBar
    - add PlayerBar, for now only support pause, play, stop
    - add MusicTile to ListView.builder to iterate musicList and show queried songs
    - add onTap functionality to play on the tapped song

--- lib/modules/music_list/view.dart
+++ lib/modules/music_list/view.dart
@@ -1,13 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/models/music.dart';
+import 'package:music_player/modules/music_list/bloc/player/player_bloc.dart';
+import 'package:music_player/modules/music_list/components/music_tile.dart';
+import 'package:music_player/modules/music_list/components/player_bar.dart';
+import 'package:music_player/modules/music_list/components/search_bar.dart';
+import 'package:music_player/modules/music_list/components/shimmer_tile.dart';
 
 class MusicListView extends StatelessWidget {
-  const MusicListView({Key? key}) : super(key: key);
+  final TextEditingController controller;
+  final List<Music> musicList;
+  final int? playingMusicId;
+  final bool isLoading;
+  final bool isPlaying;
+  final Animation<double> animation;
+  final MusicPlayerState playerState;
+  final void Function(Music) onTap;
+  final Function(Music) onResume;
+  final VoidCallback onStop;
+  final Function(Music) onPause;
+  const MusicListView({
+    Key? key,
+    required this.controller,
+    required this.musicList,
+    this.playingMusicId,
+    this.isLoading = false,
+    this.isPlaying = false,
+    required this.animation,
+    required this.playerState,
+    required this.onTap,
+    required this.onResume,
+    required this.onStop,
+    required this.onPause,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Music Player"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: MusicSearchBar(
+              hintText: "Search by Artist",
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0).copyWith(bottom: 0),
+                child: isLoading
+                    ? MusicListShimmer()
+                    : musicList.isEmpty
+                        ? Container()
+                        : ListView.builder(
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              return MusicTile(
+                                music: musicList[index],
+                                isPlaying: playingMusicId ==
+                                        musicList[index].trackId &&
+                                    isPlaying,
+                                onTap: onTap,
+                              );
+                            },
+                          ),
+              ),
+            ),
+            SizeTransition(
+              sizeFactor: animation,
+              child: PlayerBar(
+                musicPlayerState: playerState,
+                onPause: onPause,
+                onResume: onResume,
+                onStop: onStop,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
